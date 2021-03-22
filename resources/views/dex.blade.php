eeff61ae3568e52177850fae8a768e7d9696debc
Adriano Christian <adrianochristian3@gmail.com>
2021-03-22 12:09:18 -0300

    advances pokemon details page visuals

--- resources/views/dex.blade.php
+++ resources/views/dex.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.template')
 @section('content')
+<style>
+.footer-wrapper{
+    position: relative !important;
+}
+</style>
 <div class="content-wrapper">
     <ul>
     @foreach($pokemons as $p)
-        <a href="{{ route('get.pokemon', $p->name) }}"><li class="pokemon-list img-wrapper" style="background-color:{{$p->color[0]}}"><img src="{{$p->data['image']->front_default}}" class="pokemon-img"><span class="pokemon-lbl">{{ $p->name }}</span></li>
+        <a href="{{ route('get.pokemon', $p->name) }}"><li class="pokemon-list img-wrapper" style="background-color:{{$p->color[0]}}"><img src="{{$p->data['image']->front_default}}"><span class="pokemon-lbl">{{ $p->name }}</span></li>
     @endforeach
     </ul>
 </div>
